8a63f2eb1adfe18aba3e4fe8400ec9e158177ef7
doramail <doramail@users.noreply.github.com>
2023-01-10 22:12:07 +0100

    Challenge fully terminated
    Bonus terminé et opérationnel.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,32 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private bool spawned = false;
+    private float decay = 0.0f;
+    [SerializeField] float InitialisationCompteur = 3.0f; // Temps en secondes avant de laisser la possibilité d'appuyer à nouveau sur la touche ESPACE.
+
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        Reset();
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
         {
+            decay = InitialisationCompteur;
+            spawned = true;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    private void Reset()
+    {
+        if (spawned && decay > 0)
+        {
+            decay -= Time.deltaTime; // on décrémente le temps du timer
+        }
+        if (decay < 0)
+        {
+            decay = 0;
+            spawned = false;
         }
     }
 }
